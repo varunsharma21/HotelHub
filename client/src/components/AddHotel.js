10ffca0ea4a75517f2ff0643a7e734611e6ef38f
Varun Sharma <61429136+varunsharma21@users.noreply.github.com>
2024-05-14 22:02:04 +0530

    [ADD] : created better UI

--- client/src/components/AddHotel.js
+++ client/src/components/AddHotel.js
@@ -10,6 +10,8 @@ const inputInfo = [
   { label: "Contact Number", stateKey: "contactNumber" },
   { label: "Min Price", stateKey: "minPrice" },
   { label: "Max Price", stateKey: "maxPrice" },
+  { label: "Category", stateKey: "category" },
+  { label: "Star Rating", stateKey: "starRating" },
 ];
 
 const AddHotel = () => {
@@ -20,6 +22,8 @@ const AddHotel = () => {
     contactNumber: "",
     minPrice: "",
     maxPrice: "",
+    category: "",
+    starRating: "",
   };
 
   const [formData, setFormData] = useState(initialState);
@@ -34,10 +38,44 @@ const AddHotel = () => {
 
   const submitHandler = async (e) => {
     e.preventDefault();
-    // POST request
+    console.log("entered the function.", formData);
 
+    // POST request
+    try {
+      const response = await fetch("http://localhost:5500/api/v1/hotels", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        // body: JSON.stringify(formData), this will be wrong here.
+        // Bcoz backend is expecting an object with keys hotelName,address...etc.
+        body: JSON.stringify({
+          hotelName: formData.hotel,
+          address: formData.address,
+          city: formData.city,
+          contactNumber: formData.contactNumber,
+          category: formData.category,
+          starRating: formData.starRating,
+          maxPrice: formData.maxPrice,
+          minPrice: formData.minPrice,
+        }),
+      });
+
+      console.log(response);
+
+      if (!response.ok) {
+        throw Error("Network response was not ok");
+      }
+
+      const data = await response.json();
+      console.log("Data:", data);
+
+      // fetching new data again.
+    } catch (error) {
+      console.error("Error:", error.message);
+    }
     console.log(formData);
-    setFormData(initialState);
+    // setFormData(initialState);
   };
 
   return (
@@ -48,7 +86,7 @@ const AddHotel = () => {
             key={index}
             value={formData[info.stateKey]}
             onChange={(e) => handleChange(e, info.stateKey)}
-            // required={true}
+            required={true}
             id="outlined-basic"
             label={info.label}
             variant="outlined"
